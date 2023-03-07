372830bfd44703e7d4bc8d67f281bd8a1585cf95
Guilherme Teixeira <80058972+Guui-t@users.noreply.github.com>
2023-03-06 23:30:16 -0300

    Update checkIcon.tsx

--- src/icons/checkIcon.tsx
+++ src/icons/checkIcon.tsx
@@ -1,36 +1,49 @@
-import { createIcon } from '@chakra-ui/react';
+import { createIcon } from "@chakra-ui/react";
+
 export const RadioCheckUnchecked = createIcon({
-  displayName: 'RadioCheckUnchecked',
-  viewBox: '0 0 24 24',
+  displayName: "RadioCheckUnchecked",
+  viewBox: "0 0 24 24",
   path: [
-    <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="5.1287" y1="18.8713" x2="19.2687" y2="4.7313">
+    <linearGradient
+      id="SVGID_1_"
+      gradientUnits="userSpaceOnUse"
+      x1="5.1287"
+      y1="18.8713"
+      x2="19.2687"
+      y2="4.7313"
+    >
       <stop offset="0.111" stopColor="#3272A1" />
       <stop offset="0.8797" stopColor="#CF00AA" />
     </linearGradient>,
 
-    <path d='M0 0h24v24H0V0z' fill='none' />,
-
+    <path d="M0 0h24v24H0V0z" fill="none" />,
 
     <path
-      fill='url(#SVGID_1_)'
-
-      d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z'
+      fill="url(#SVGID_1_)"
+      d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"
     />,
   ],
 });
 
 export const RadioCheckChecked = createIcon({
-  displayName: 'RadioCheckChecked',
-  viewBox: '0 0 24 24',
+  displayName: "RadioCheckChecked",
+  viewBox: "0 0 24 24",
   path: [
-    <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="5.1287" y1="18.8713" x2="19.2687" y2="4.7313">
+    <linearGradient
+      id="SVGID_1_"
+      gradientUnits="userSpaceOnUse"
+      x1="5.1287"
+      y1="18.8713"
+      x2="19.2687"
+      y2="4.7313"
+    >
       <stop offset="0.111" stopColor="#3272A1" />
       <stop offset="0.8797" stopColor="#CF00AA" />
     </linearGradient>,
-    <path d='M0 0h24v24H0V0z' fill='none' />,
+    <path d="M0 0h24v24H0V0z" fill="none" />,
     <path
-      fill='url(#SVGID_1_)'
-      d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM9.29 16.29 5.7 12.7c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L10 14.17l6.88-6.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-7.59 7.59c-.38.39-1.02.39-1.41 0z'
+      fill="url(#SVGID_1_)"
+      d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM9.29 16.29 5.7 12.7c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L10 14.17l6.88-6.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-7.59 7.59c-.38.39-1.02.39-1.41 0z"
     />,
   ],
 });
